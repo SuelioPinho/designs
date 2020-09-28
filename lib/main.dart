999b0362c8f6f647f86143a1f613a88b95a36d96
Suelio Pss <rav@Ravs-Mac-mini.local>
2020-09-28 11:33:53 -0300

    Scroll Page

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Designs',
-      initialRoute: 'basic',
+      initialRoute: 'scroll',
       routes: {
         'basic': (BuildContext context) => BasicPage(),
         'scroll': (BuildContext context) => ScrollPage(),
